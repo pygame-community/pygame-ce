00da7d380093a09c2124662311b5a79929922a7b
Dan Lawrence <danintheshed@gmail.com>
2023-02-18 08:10:00 +0000

    Delete old unused scrap module code

--- src_c/scrap.c
+++ src_c/scrap.c
@@ -331,7 +331,6 @@ _scrap_set_mode(PyObject *self, PyObject *args)
     Py_RETURN_NONE;
 }
 
-
 static PyMethodDef scrap_builtins[] = {
 /*
  * Only initialise these functions for ones we know about.
