a1be68dc4133c4aac8410c7ac58b5459740c29a0
yunline <yunline@foxmail.com>
2023-03-29 20:18:36 +0800

    fix error

--- src_c/include/_pygame.h
+++ src_c/include/_pygame.h
@@ -470,7 +470,7 @@ typedef struct {
 #ifndef PYGAMEAPI_WINDOW_INTERNAL
 #define pgWindow_Type (*(PyTypeObject *)PYGAMEAPI_GET_SLOT(window, 0))
 #define pgWindow_Check(x) \
-    (PyObject_IsInstance((x), (PyTypeObject *)&pgWindow_Type))
+    (PyObject_IsInstance((x), (PyObject *)&pgWindow_Type))
 #define import_pygame_window() IMPORT_PYGAME_MODULE(window)
 #endif
 
