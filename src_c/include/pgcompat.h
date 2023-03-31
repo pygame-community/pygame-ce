42e9fd79b7e817001828d8d856410f47971f2979
yunline <yunline@foxmail.com>
2023-03-31 15:38:31 +0800

    add NO_SDL_MOUSEWHEEL_FLIPPED back

--- src_c/include/pgcompat.h
+++ src_c/include/pgcompat.h
@@ -31,6 +31,10 @@ typedef uint8_t Uint8;
 #define PG_GetTicks SDL_GetTicks
 #endif /* SDL_VERSION_ATLEAST(2, 0, 18) */
 
+#ifndef SDL_MOUSEWHEEL_FLIPPED
+#define NO_SDL_MOUSEWHEEL_FLIPPED
+#endif
+
 #endif /* defined(SDL_VERSION_ATLEAST) */
 
 #endif /* ~defined(PGCOMPAT_H) */
