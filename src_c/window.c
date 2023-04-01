dd4ea5f0535a5ff4f359c51316b883a1b973616d
yunline <yunline@foxmail.com>
2023-04-01 13:00:34 +0800

    fix bug

--- src_c/window.c
+++ src_c/window.c
@@ -871,20 +871,6 @@ MODINIT_DEFINE(window)
     _window_list = PyList_New(0);
     Py_XINCREF(_window_list);
 
-    PyObject *WINDOWPOS_UNDEFINED = PyLong_FromLong(SDL_WINDOWPOS_UNDEFINED);
-    if (PyModule_AddObject(module, "WINDOWPOS_UNDEFINED",
-                           WINDOWPOS_UNDEFINED)) {
-        Py_DECREF(WINDOWPOS_UNDEFINED);
-        Py_DECREF(module);
-        return NULL;
-    }
-    PyObject *WINDOWPOS_CENTERED = PyLong_FromLong(SDL_WINDOWPOS_CENTERED);
-    if (PyModule_AddObject(module, "WINDOWPOS_CENTERED", WINDOWPOS_CENTERED)) {
-        Py_DECREF(WINDOWPOS_CENTERED);
-        Py_DECREF(module);
-        return NULL;
-    }
-
     SDL_AddEventWatch(_resize_event_watch, NULL);
 
     _dummy_surface =
