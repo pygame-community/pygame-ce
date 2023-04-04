2b887bb0281d9111e066a19a448a1f90203bbfce
Nari Lee <nari.lee2006@gmail.com>
2023-04-04 04:30:30 -0400

    Redirect home_uri to pyga.me/docs

--- docs/reST/conf.py
+++ docs/reST/conf.py
@@ -106,7 +106,7 @@ html_theme = 'classic'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {'home_uri': 'https://pyga.me'}
+html_theme_options = {'home_uri': 'https://pyga.me/docs'}
 
 # Add any paths that contain custom themes here, relative to this directory.
 html_theme_path = ['themes']
