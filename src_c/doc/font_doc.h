2f93a160d2c2761b2f73959411fbdace55c1d1a0
Robert O'Shea <PurityLake@users.noreply.github.com>
2023-05-17 04:21:31 +0100

    Added keyword arguments to `Font.render` (#2000)
    * Added kwargs to font.render
    
    * Added docs and tests for kwargs for font.render
    
    * Fix incompatible pointer
    
    * Added wraplength to font.render
    
    * Attempt to fix failing test
    
    * Remove changes to ftfont.py
    
    * Change a test to only run for FontTest class
    
    * Fix formatting issue
    
    * Adjusted keyword arg names for font.render and updated docs
    
    * Fix documentation error
    
    * Changed arguments fgcolor and bgcolor in ftfont
    
    * Adjusted tests to include freetype font
    
    * Adjusted formatting
    
    * Fixed failing test in font_test
    
    * Fixing font_test again
    
    * Fixing font_test yet again
    
    * Fix formatting
    
    * Renamed fgcolor to color
    
    * Fixed remaining mentions of background

--- src_c/doc/font_doc.h
+++ src_c/doc/font_doc.h
@@ -15,7 +15,7 @@
 #define DOC_FONT_FONT_UNDERLINE "underline -> bool\nGets or sets whether the font should be rendered with an underline."
 #define DOC_FONT_FONT_STRIKETHROUGH "strikethrough -> bool\nGets or sets whether the font should be rendered with a strikethrough."
 #define DOC_FONT_FONT_ALIGN "align -> int\nSet how rendered text is aligned when given a wrap length"
-#define DOC_FONT_FONT_RENDER "render(text, antialias, color, background=None, wraplength=0) -> Surface\ndraw text on a new Surface"
+#define DOC_FONT_FONT_RENDER "render(text, antialias, color, bgcolor=None, wraplength=0) -> Surface\ndraw text on a new Surface"
 #define DOC_FONT_FONT_SIZE "size(text) -> (width, height)\ndetermine the amount of space needed to render text"
 #define DOC_FONT_FONT_SETUNDERLINE "set_underline(bool) -> None\ncontrol if text is rendered with an underline"
 #define DOC_FONT_FONT_GETUNDERLINE "get_underline() -> bool\ncheck if text will be rendered with an underline"
