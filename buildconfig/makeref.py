db3917eb22292b9f2fc346cbe1ff7e3c344ecacb
Notenlish <71970100+Notenlish@users.noreply.github.com>
2023-05-21 20:10:12 +0300

    revert to single quotes

--- buildconfig/makeref.py
+++ buildconfig/makeref.py
@@ -14,25 +14,25 @@ c_header_dir = os.path.join('src_c', 'doc')
 def run():
     full_generation_flag = False
     for argument in sys.argv[1:]:
-        if argument == "full_generation":
+        if argument == 'full_generation':
             full_generation_flag = True
     try:
-        subprocess_args = [sys.executable, "-m", "sphinx",
-                           "-b", "html",
-                           "-d", rst_doctree_dir,
-                           "-D", f"headers_dest={c_header_dir}",
-                           "-D", "headers_mkdirs=0",
+        subprocess_args = [sys.executable, '-m', 'sphinx',
+                           '-b', 'html',
+                           '-d', rst_doctree_dir,
+                           '-D', f'headers_dest={c_header_dir}',
+                           '-D', 'headers_mkdirs=0',
                            rst_source_dir, rst_build_dir, ]
         if full_generation_flag:
-            subprocess_args.append("-E")
-        print("Executing sphinx in subprocess with args:", subprocess_args)
+            subprocess_args.append('-E')
+        print('Executing sphinx in subprocess with args:', subprocess_args)
         return subprocess.run(subprocess_args).returncode
     except Exception:
-        print("---")
-        print("Have you installed sphinx?")
-        print("---")
+        print('---')
+        print('Have you installed sphinx?')
+        print('---')
         raise
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     sys.exit(run())
