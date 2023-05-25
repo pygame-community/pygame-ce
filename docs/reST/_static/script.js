2ddc9c8f3bb806c7f139f0aed6fb3f3d5736779b
Notenlish <71970100+Notenlish@users.noreply.github.com>
2023-05-25 20:48:45 +0300

    move searchbar to svg files, fix searchbar button not working, reverse the way data-theme works

--- docs/reST/_static/script.js
+++ docs/reST/_static/script.js
@@ -1,3 +1,13 @@
+const search_buttons = document.querySelectorAll('.searchbar-button');
+const search_submit = document.querySelector('.searchbar-submit');
+
+// When the icon is clicked, submit the search form
+search_buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        search_submit.click();
+    });
+});
+
 if (localStorage.getItem('theme') === null) {
     localStorage.setItem('theme', 'light-theme');
     document.body.classList.add('light-theme');
@@ -9,8 +19,8 @@ const theme_icons = document.querySelectorAll('.theme-icon');
 theme_icons.forEach((icon) => {
     icon.addEventListener('click', () => {
         const theme = icon.getAttribute('data-theme');
-        document.body.classList.remove('light-theme', 'dark-theme');
-        document.body.classList.add(theme);
-        localStorage.setItem('theme', theme);
+        document.body.classList.add('light-theme', 'dark-theme');
+        document.body.classList.remove(theme);
+        localStorage.setItem('theme', document.body.classList[0]);
     });
 });
