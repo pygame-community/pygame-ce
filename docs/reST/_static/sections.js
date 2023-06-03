fe2cab5bdef564cac48bb1453d5ec94bf32c24a3
NOTMEE12 <legominefan@gmail.com>
2023-06-03 19:41:10 +0200

    opening/closing sections is saved.

--- docs/reST/_static/sections.js
+++ docs/reST/_static/sections.js
@@ -1,3 +1,22 @@
-const page = document
-
-page.
+function toggleSectionVisibility(section){
+	document.getElementById(section).hidden = ! document.getElementById(section).hidden;
+	if (document.getElementById(section).hidden === true){
+		items = localStorage.getItem('hidden');
+		if (items !== null){
+			if (!(items.includes(section))){
+				items = items + '|' + section + '|';
+			}
+			localStorage.setItem('hidden', items);
+		} else {
+			localStorage.setItem('hidden', section);
+		}
+	} else {
+		items = localStorage.getItem('hidden');
+		if (items !== null){
+			if (items.includes(section)){
+				items = items.replace(section + '|', '')
+			}
+			localStorage.setItem('hidden', items);
+		}
+	}
+}
