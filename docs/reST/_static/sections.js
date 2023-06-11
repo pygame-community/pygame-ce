2268e2b8cd899e21b44459a74d56062dffc1b393
NOTMEE12 <legominefan@gmail.com>
2023-06-11 12:38:14 +0200

    approving changes

--- docs/reST/_static/sections.js
+++ docs/reST/_static/sections.js
@@ -1,23 +1,23 @@
 function toggleSectionVisibility(section, message){
 	document.getElementById(section).hidden = ! document.getElementById(section).hidden;
 	if (document.getElementById(section).hidden === true){ // show
-		items = localStorage.getItem('hidden');
+		items = sessionStorage.getItem('hidden');
 		if (items !== null){
 			if (!(items.includes(section))){
 				items = items + section + '|';
 			}
-			localStorage.setItem('hidden', items);
+			sessionStorage.setItem('hidden', items);
 		} else {
-			localStorage.setItem('hidden', section + '|');
+			sessionStorage.setItem('hidden', section + '|');
 		}
 		document.getElementById(section[0]).innerHTML = document.getElementById(section[0]).innerHTML.replace('▼', '▲');
 	} else {  // hide
-		items = localStorage.getItem('hidden');
+		items = sessionStorage.getItem('hidden');
 		if (items !== null){
 			if (items.includes(section)){
 				items = items.replace(section + '|', '');
 			}
-			localStorage.setItem('hidden', items);
+			sessionStorage.setItem('hidden', items);
 		}
 		document.getElementById(section[0]).innerHTML = document.getElementById(section[0]).innerHTML.replace('▲', '▼');
 	}
