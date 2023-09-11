5f543e030216dfc1e06310f22c5e77045cdf4f41
yunline <yunline@foxmail.com>
2023-09-11 09:43:45 +0800

    Add interactive test

--- test/message_box_test.py
+++ test/message_box_test.py
@@ -31,5 +31,65 @@ class MessageBoxTest(unittest.TestCase):
         self.assertRaises(TypeError, lambda: mb("", parent_window=123456))
 
 
+class MessageBoxInteractiveTest(unittest.TestCase):
+    __tags__ = ["interactive"]
+
+    def test_message_box_type(self):
+        result = pygame.display.message_box(
+            "Test",
+            "Is this an error message box?",
+            type="error",
+            buttons=("Yes", "No"),
+        )
+        self.assertEqual(result, 0)
+
+        result = pygame.display.message_box(
+            "Test",
+            "Is this an info message box?",
+            type="info",
+            buttons=("Yes", "No"),
+        )
+        self.assertEqual(result, 0)
+
+        result = pygame.display.message_box(
+            "Test",
+            "Is this a warn message box?",
+            type="warn",
+            buttons=("Yes", "No"),
+        )
+        self.assertEqual(result, 0)
+
+    def test_message_box_buttons(self):
+        result = pygame.display.message_box("Hit the 'OK' button")
+        self.assertEqual(result, 0)
+
+        result = pygame.display.message_box(
+            "Hit the 'Hello' button",
+            buttons=("Nope", "Nope", "Nope", "Hello", "Nope", "Nope"),
+        )
+        self.assertEqual(result, 3)
+
+        result = pygame.display.message_box(
+            "Press Enter on your keyboard",
+            buttons=("O", "O", "O", "O", "O", "O"),
+            return_button=4,
+        )
+        self.assertEqual(result, 4)
+
+        result = pygame.display.message_box(
+            "Press Esc on your keyboard",
+            buttons=("O", "O", "O", "O", "O", "O"),
+            escape_button=2,
+        )
+        self.assertEqual(result, 2)
+
+        result = pygame.display.message_box(
+            "Test",
+            "You saw 'Yes' on the left and 'No' on the right. Is this correct?",
+            buttons=("Yes", "No"),
+        )
+        self.assertEqual(result, 0)
+
+
 if __name__ == "__main__":
     unittest.main()
