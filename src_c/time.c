fb56c29cb7ab410afbb10e4076f3d87fa65ac40d
Andrew Coffey <andrewryancoffey@hotmail.com>
2023-09-24 09:36:25 -0500

    use SDL_MAX_UINT64 instead of custom definition

--- src_c/time.c
+++ src_c/time.c
@@ -27,7 +27,6 @@
 #include "doc/time_doc.h"
 
 #define WORST_CLOCK_ACCURACY 12
-#define MAX_UINT_64 18446744073709551615ULL
 
 /* Enum containing some error codes used by timer related functions */
 typedef enum {
@@ -331,7 +330,7 @@ accurate_delay(Sint64 ticks)
     }
     do {
         delay = ticks - (PG_GetTicks() - funcstart);
-    } while (delay > 0 && delay < (MAX_UINT_64 >> 1));
+    } while (delay > 0 && delay < (SDL_MAX_UINT64 >> 1));
 
     return PG_GetTicks() - funcstart;
 }
