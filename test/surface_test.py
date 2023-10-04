7d8b03195fa282f9f3beb88e960995f0572fdd06
JorasOliveira <joras.custodio@gmail.com>
2023-10-04 08:23:54 -0300

    isolated test that only works when not using pypy

--- test/surface_test.py
+++ test/surface_test.py
@@ -628,8 +628,9 @@ class SurfaceTypeTest(unittest.TestCase):
         self.assertIsInstance(rect, pygame.Rect)
         self.assertEqual(rect_kwargs.center, (100, 100))
         self.assertNotEqual(rect_kwargs.topleft, (1.0, 1.0))
-        with self.assertRaises(AttributeError):
-            surf.get_rect(centre=(100, 100))
+        if not IS_PYPY: #PyPy doesn't raise an AttributeError, so for PyPy we can just skip it.
+            with self.assertRaises(AttributeError):
+                surf.get_rect(centre=(100, 100))
         
     ########################################################################
 
@@ -645,8 +646,9 @@ class SurfaceTypeTest(unittest.TestCase):
         self.assertIsInstance(frect, pygame.FRect)
         self.assertEqual(frect_kwargs.center, (100, 100))
         self.assertNotEqual(frect_kwargs.topleft, (1.0, 1.0))
-        with self.assertRaises(AttributeError):
-            surf.get_frect(centre=(100, 100))
+        if not IS_PYPY: #PyPy doesn't raise an AttributeError, so for PyPy we can just skip it.
+            with self.assertRaises(AttributeError):
+                surf.get_frect(centre=(100, 100))
 
     ########################################################################
 
