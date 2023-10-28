75651d35ba617a57a0634399f35762279fcf21ed
Ankith <itsankith26@gmail.com>
2023-10-28 19:14:08 +0530

    Update pygame.version to not be an autogen file

--- src_c/include/_pygame.h
+++ src_c/include/_pygame.h
@@ -58,6 +58,7 @@
 #define PG_MAJOR_VERSION 2
 #define PG_MINOR_VERSION 4
 #define PG_PATCH_VERSION 0
+#define PG_VERSION_TAG "dev3"
 #define PG_VERSIONNUM(MAJOR, MINOR, PATCH) \
     (1000 * (MAJOR) + 100 * (MINOR) + (PATCH))
 #define PG_VERSION_ATLEAST(MAJOR, MINOR, PATCH)                             \
