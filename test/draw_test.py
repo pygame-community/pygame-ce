3e364cd547bc77120de7777467449da3a5f5b7fc
Houda222 <houda.ghallab@imt-atlantique.net>
2023-12-26 18:38:03 +0100

    added todo_test_arc

--- test/draw_test.py
+++ test/draw_test.py
@@ -6160,9 +6160,55 @@ class DrawArcMixin:
             with self.assertRaises(TypeError):
                 bounds_rect = self.draw_arc(**kwargs)
 
-    def todo_test_arc(self):
+    def test_arc__correct_drawing(self):
         """Ensure draw arc works correctly."""
-        self.fail()
+        surfw, surfh = 500, 500
+        surface = pygame.Surface((surfw, surfh))
+        surface_color = pygame.Color("black")
+        surface.fill(surface_color)
+        arc_color = pygame.Color("white")
+        rectangles = [
+            pygame.Rect((200, 200), (300 + i, 200 + i)) for i in range(-100, 50, 30)
+        ]  # Test on different bounding rectangles
+        start = 0
+        stop = 2
+
+        for rect in rectangles:
+            kwargs = {
+                "surface": surface,
+                "color": arc_color,
+                "rect": rect,
+                "start_angle": start,
+                "stop_angle": stop,
+                "width": 1,
+            }
+
+            pygame.draw.arc(**kwargs)
+
+            a, b = rect.width / 2, rect.height / 2
+            number_of_valid_arc_points = 0
+            number_of_invalid_arc_points = 0
+            # Define a threshold for comparison
+            eps = 0.1
+            center_x = rect.centerx
+            center_y = rect.centery
+
+            for x in range(surfw):
+                for y in range(surfh):
+                    # Check whether the point on the arc belongs to the ellipse defined by the bounding rectangle
+                    if (surface.get_at((x, y)) == arc_color) and abs(
+                        ((x - center_x) / a) ** 2 + ((y - center_y) / b) ** 2 - 1
+                    ) <= eps:
+                        number_of_valid_arc_points += 1
+                    elif surface.get_at((x, y)) == arc_color:
+                        number_of_invalid_arc_points += 1
+
+            surface.fill(surface_color)  # Clear for the next test
+
+            self.assertEqual(
+                number_of_valid_arc_points,
+                number_of_invalid_arc_points + number_of_valid_arc_points,
+            )
 
     def test_arc__bounding_rect(self):
         """Ensures draw arc returns the correct bounding rect.
