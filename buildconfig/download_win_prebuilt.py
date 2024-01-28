e93dd7c1c3091e365b4a81e767657189f7ab310e
Starbuck5 <46412508+Starbuck5@users.noreply.github.com>
2024-01-28 00:51:20 -0800

    2.29.3

--- buildconfig/download_win_prebuilt.py
+++ buildconfig/download_win_prebuilt.py
@@ -78,8 +78,8 @@ def get_urls(x86=True, x64=True):
     url_sha1 = []
     url_sha1.extend([
         [
-        'https://github.com/libsdl-org/SDL/releases/download/prerelease-2.29.2/SDL2-devel-2.29.2-VC.zip',
-        '338870018bca5361829cba2d7e5f0a9260ca39c5',
+        'https://github.com/libsdl-org/SDL/releases/download/prerelease-2.29.3/SDL2-devel-2.29.3-VC.zip',
+        'a9eddd318ee0c1fe6fbe77db4688fde55b594312',
         ],
         [
         'https://github.com/pygame-community/SDL_image/releases/download/2.8.2-pgce/SDL2_image-devel-2.8.2-VCpgce.zip',
@@ -234,12 +234,12 @@ def place_downloaded_prebuilts(temp_dir, move_to_dir, x86=True, x64=True):
         copy(
             os.path.join(
                 temp_dir,
-                'SDL2-devel-2.29.2-VC/SDL2-2.29.2'
+                'SDL2-devel-2.29.3-VC/SDL2-2.29.3'
             ),
             os.path.join(
                 move_to_dir,
                 prebuilt_dir,
-                'SDL2-2.29.2'
+                'SDL2-2.29.3'
             )
         )
 
