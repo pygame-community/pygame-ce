fc0d76e012eaa5139c5b31b9444c9667f7593948
Andrew Coffey <andrewryancoffey@hotmail.com>
2024-02-03 16:13:00 -0600

    bump windows and mac deps to SDL 2.30.0 release

--- buildconfig/download_win_prebuilt.py
+++ buildconfig/download_win_prebuilt.py
@@ -78,8 +78,8 @@ def get_urls(x86=True, x64=True):
     url_sha1 = []
     url_sha1.extend([
         [
-        'https://github.com/libsdl-org/SDL/releases/download/prerelease-2.29.3/SDL2-devel-2.29.3-VC.zip',
-        'a9eddd318ee0c1fe6fbe77db4688fde55b594312',
+        'https://github.com/libsdl-org/SDL/releases/download/release-2.30.0/SDL2-devel-2.30.0-VC.zip',
+        'cc0cc9ce3526bdec53cf314e499b4d822a9af5d4',
         ],
         [
         'https://github.com/pygame-community/SDL_image/releases/download/2.8.2-pgce/SDL2_image-devel-2.8.2-VCpgce.zip',
@@ -234,12 +234,12 @@ def place_downloaded_prebuilts(temp_dir, move_to_dir, x86=True, x64=True):
         copy(
             os.path.join(
                 temp_dir,
-                'SDL2-devel-2.29.3-VC/SDL2-2.29.3'
+                'SDL2-devel-2.30.0-VC/SDL2-2.30.0'
             ),
             os.path.join(
                 move_to_dir,
                 prebuilt_dir,
-                'SDL2-2.29.3'
+                'SDL2-2.30.0'
             )
         )
 
