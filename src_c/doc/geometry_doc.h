ef0bd76696144e503bffc7cbe3ca13efe6bbe2f7
ScriptLineStudios <scriptlinestudios@protonmail.com>
2024-04-21 09:46:19 +0200

    python setup.py docs removed this?

--- src_c/doc/geometry_doc.h
+++ src_c/doc/geometry_doc.h
@@ -1,4 +1,5 @@
 /* Auto generated file: with makeref.py .  Docs go in docs/reST/ref/ . */
+#define DOC_GEOMETRY "pygame module for the Circle, Line, and Polygon objects"
 #define DOC_CIRCLE "Circle((x, y), radius) -> Circle\nCircle(x, y, radius) -> Circle\npygame object for representing a circle"
 #define DOC_CIRCLE_X "x -> float\ncenter x coordinate of the circle"
 #define DOC_CIRCLE_Y "y -> float\ncenter y coordinate of the circle"
