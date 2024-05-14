c348a967c83f04efd9643d912d221e2e4df95033
Damus2D <macchia.cuordileone@gmail.com>
2024-05-15 00:16:24 +0200

    Remove spaces added by clang-format

--- src_c/surface.c
+++ src_c/surface.c
@@ -3,13 +3,13 @@
   Copyright (C) 2000-2001  Pete Shinners
   Copyright (C) 2007 Marcus von Appen
 
-  This library is free software; you can redistribute it and/or   
-  modify it under the terms of the GNU Library General Public     
-  License as published by the Free Software Foundation; either    
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Library General Public
+  License as published by the Free Software Foundation; either
   version 2 of the License, or (at your option) any later version.
 
-  This library is distributed in the hope that it will be useful, 
-  but WITHOUT ANY WARRANTY; without even the implied warranty of  
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
   Library General Public License for more details.
 
