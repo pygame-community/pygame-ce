84541ffa27247e3a99aae4683aca501191bb2fed
Andrew Coffey <andrewryancoffey@hotmail.com>
2024-05-20 15:46:28 -0500

    Rebased with main

--- buildconfig/manylinux-build/build-wheels.sh
+++ buildconfig/manylinux-build/build-wheels.sh
@@ -40,7 +40,7 @@ for PYVER in $SUPPORTED_PYTHONS; do
 	    PYTHON="/opt/python/${PYVER}/bin/pypy"
 	fi
 
-    ${PYTHON} -m pip install Sphinx
+    ${PYTHON} -m pip install -r requirements-dev.txt
     cd io
     ${PYTHON} setup.py docs
     cd ..
