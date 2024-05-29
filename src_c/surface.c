9009783934a733f728dab5481042cfeedd8091ee
itzpr3d4t0r <103119829+itzpr3d4t0r@users.noreply.github.com>
2024-05-29 15:16:08 +0200

    rule out rle

--- src_c/surface.c
+++ src_c/surface.c
@@ -3934,7 +3934,9 @@ pgSurface_Blit(pgSurfaceObject *dstobj, pgSurfaceObject *srcobj,
         result = pygame_Blit(src, srcrect, dst, dstrect, blend_flags);
     }
     else if (!SDL_GetSurfaceBlendMode(src, &blend_mode) &&
-             blend_mode == SDL_BLENDMODE_BLEND) {
+             blend_mode == SDL_BLENDMODE_BLEND && !pg_HasSurfaceRLE(src) &&
+             !pg_HasSurfaceRLE(dst) && !(src->flags & SDL_RLEACCEL) &&
+             !(dst->flags & SDL_RLEACCEL)) {
         result = pygame_Blit(src, srcrect, dst, dstrect, blend_flags);
     }
     else {
