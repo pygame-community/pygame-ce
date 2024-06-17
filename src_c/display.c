93828b93d3e657937d6ae582ae6a5b8a2cf432ae
gresm <78505251+gresm@users.noreply.github.com>
2024-06-17 19:58:42 +0200

    Just rename "event.c" to "_event.c".

--- src_c/display.c
+++ src_c/display.c
@@ -194,7 +194,7 @@ pg_display_quit(PyObject *self, PyObject *_null)
         pg_SetDefaultWindow(NULL);
     }
 
-    pg_mod_autoquit(IMPPREFIX "event");
+    pg_mod_autoquit(IMPPREFIX "_event");
     pg_mod_autoquit(IMPPREFIX "time");
     pg_mod_autoquit(IMPPREFIX "window");
 
@@ -250,7 +250,7 @@ pg_display_init(PyObject *self, PyObject *_null)
 
     if (!pg_mod_autoinit(IMPPREFIX "time"))
         return NULL;
-    if (!pg_mod_autoinit(IMPPREFIX "event"))
+    if (!pg_mod_autoinit(IMPPREFIX "_event"))
         return NULL;
     if (!pg_mod_autoinit(IMPPREFIX "window"))
         return NULL;
---
+++ src_py/event.py
@@ -0,0 +1,1 @@
+from pygame._event import *
