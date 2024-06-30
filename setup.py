db039343ac80e775e9863fa455186769b74697af
Andrew Coffey <49015102+oddbookworm@users.noreply.github.com>
2024-06-30 10:58:26 -0700

    Docs generation is now `python -m buildconfig docs` and `python -m buildconfig docs full_generation` (#2942)
    * Can now use python -m buildconfig docs full_generation to generate docs instead of calling make_docs.py directly
    
    * Fixed buildconfig command to be a module run
    
    * Updated docs readme to point to new command
    
    * Stricter command interface (thanks Ankith)
    
    Co-authored-by: Ankith <itsankith26@gmail.com>
    
    ---------
    
    Co-authored-by: Ankith <itsankith26@gmail.com>

--- setup.py
+++ setup.py
@@ -124,7 +124,7 @@ distutils.ccompiler.CCompiler.__spawn = distutils.ccompiler.CCompiler.spawn
 distutils.ccompiler.CCompiler.spawn = spawn
 
 # A (bit hacky) fix for https://github.com/pygame-community/pygame-ce/issues/1346
-# This is due to the fact that distutils uses command line args to 
+# This is due to the fact that distutils uses command line args to
 # export PyInit_* functions on windows, but those functions are already exported
 # and that is why compiler gives warnings
 from distutils.command.build_ext import build_ext
@@ -264,7 +264,7 @@ if compile_cython:
     for i, kwargs in enumerate(queue):
         kwargs['progress'] = f'[{i + 1}/{count}] '
         cythonize_one(**kwargs)
-    
+
     if cython_only:
         sys.exit(0)
 
@@ -420,7 +420,7 @@ for e in extensions:
 
     if "freetype" in e.name and sys.platform not in ("darwin", "win32"):
         # TODO: fix freetype issues here
-        if sysconfig.get_config_var("MAINCC") != "clang":        
+        if sysconfig.get_config_var("MAINCC") != "clang":
             e.extra_compile_args.append("-Wno-error=unused-but-set-variable")
 
     if "mask" in e.name and sys.platform == "win32":
@@ -806,13 +806,13 @@ class DocsCommand(Command):
         '''
         import subprocess
         command_line = [
-            sys.executable, os.path.join('buildconfig', 'make_docs.py')
+            sys.executable, "-m", "buildconfig", "docs"
         ]
         if self.fullgeneration:
             command_line.append('full_generation')
 
         print("WARNING: This command is deprecated and will be removed in the future.")
-        print(f"Please use the following replacement: `{' '.join(command_line)}`\n")  
+        print(f"Please use the following replacement: `{' '.join(command_line)}`\n")
         if subprocess.call(command_line) != 0:
             raise SystemExit("Failed to build documentation")
 
@@ -823,7 +823,7 @@ class StubcheckCommand(Command):
     user_options = []
     def initialize_options(self):
         pass
-        
+
     def finalize_options(self):
         pass
 
@@ -836,7 +836,7 @@ class StubcheckCommand(Command):
             sys.executable, os.path.join("buildconfig", "stubs", "stubcheck.py")
         ]
         print("WARNING: This command is deprecated and will be removed in the future.")
-        print(f"Please use the following replacement: `{' '.join(command_line)}`\n")  
+        print(f"Please use the following replacement: `{' '.join(command_line)}`\n")
         result = subprocess.run(command_line)
         if result.returncode != 0:
             raise SystemExit("Stubcheck failed.")
