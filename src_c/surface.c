0ccf76761645b72b8e7a55b7b5ed07acd07fcbd2
Damiano Ricciardi <macchia.cuordileone@gmail.com>
2024-07-03 13:35:05 +0200

    Add a version check for format BGR444

--- src_c/surface.c
+++ src_c/surface.c
@@ -3173,7 +3173,9 @@ surf_get_pixel_format(pgSurfaceObject *self, PyObject *_null)
             pixel_format = "RGB";
             break;
 
+#if SDL_VERSION_ATLEAST(1, 0, 12)
         case SDL_PIXELFORMAT_BGR444:
+#endif
         case SDL_PIXELFORMAT_BGR555:
         case SDL_PIXELFORMAT_BGR565:
         case SDL_PIXELFORMAT_BGR24:
