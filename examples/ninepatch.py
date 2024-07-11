2df32652b3733e6e891cad9bad5ca5900dc233d6
Damiano Ricciardi <macchia.cuordileone@gmail.com>
2024-07-11 23:03:17 +0200

    Pre-commit didn't work the first time

--- examples/ninepatch.py
+++ examples/ninepatch.py
@@ -23,13 +23,14 @@ SCREEN_SIZE = (600, 500)
 SCALE_SIZE = (500, 150)
 CORNER_SIZE = 20
 
+
 def ninepatch_scale(
     surface: pygame.Surface,
     size: typing.Sequence[int],
     corner_size: int,
     alpha: bool = True,
     smooth: bool = False,
-    dest_surface: pygame.Surface | None = None
+    dest_surface: pygame.Surface | None = None,
 ) -> pygame.Surface:
     # the code of this function also emulates how a transform-like ninepatch function would likely behave
     if dest_surface is not None:
@@ -37,67 +38,68 @@ def ninepatch_scale(
         if ret_surface.size != size:
             raise ValueError("Destination surface doesn't match the provided size")
     else:
-        ret_surface = pygame.Surface(size, pygame.SRCALPHA*alpha) # when alpha is False the flags become 0
-    
+        ret_surface = pygame.Surface(
+            size, pygame.SRCALPHA * alpha
+        )  # when alpha is False the flags become 0
+
     # aliases
-    ret_w, ret_h = size # non-sequence argument catched by python
+    ret_w, ret_h = size  # non-sequence argument catched by python
     src_w, src_h = surface.size
     c = corner_size
     scale_func = pygame.transform.smoothscale if smooth else pygame.transform.scale
-    subsurface = surface.subsurface # alias for performance/clarity
-    
+    subsurface = surface.subsurface  # alias for performance/clarity
+
     if corner_size < 0 or corner_size > min(ret_w, ret_h):
-        raise ValueError("Corner size must be nonnegative and not greater than the smaller between width and height")
-    if corner_size == 0: # default to normal scaling
+        raise ValueError(
+            "Corner size must be nonnegative and not greater than the smaller between width and height"
+        )
+    if corner_size == 0:  # default to normal scaling
         return scale_func(surface, size)
-    
-    src_x_side, ret_x_side = src_w-c*2, ret_w-c*2
-    src_y_side, ret_y_side = src_h-c*2, ret_h-c*2
-    
-    ret_surface.blit( # topleft corner
-        subsurface(0, 0, c, c),
-        (0, 0))
-    ret_surface.blit( # topright corner
-        subsurface(src_w-c, 0, c, c),
-        (ret_w-c, 0))
-    ret_surface.blit( # bottomleft corner
-        subsurface(0, src_h-c, c, c),
-        (0, ret_h-c))
-    ret_surface.blit( # bottomright corner
-        subsurface(src_w-c, src_h-c, c, c),
-        (ret_w-c, ret_h-c))
-    
+
+    src_x_side, ret_x_side = src_w - c * 2, ret_w - c * 2
+    src_y_side, ret_y_side = src_h - c * 2, ret_h - c * 2
+
+    ret_surface.blit(  # topleft corner
+        subsurface(0, 0, c, c), (0, 0)
+    )
+    ret_surface.blit(  # topright corner
+        subsurface(src_w - c, 0, c, c), (ret_w - c, 0)
+    )
+    ret_surface.blit(  # bottomleft corner
+        subsurface(0, src_h - c, c, c), (0, ret_h - c)
+    )
+    ret_surface.blit(  # bottomright corner
+        subsurface(src_w - c, src_h - c, c, c), (ret_w - c, ret_h - c)
+    )
+
     if src_x_side > 0:
-        ret_surface.blit( # top side
-            scale_func(
-                subsurface(c, 0, src_x_side, c), 
-                (ret_x_side, c)), 
-            (c, 0))
-        ret_surface.blit( # bottom side
-            scale_func(
-                subsurface(c, src_h-c, src_x_side, c),
-                (ret_x_side, c)),
-            (c, ret_h-c))
-        
+        ret_surface.blit(  # top side
+            scale_func(subsurface(c, 0, src_x_side, c), (ret_x_side, c)), (c, 0)
+        )
+        ret_surface.blit(  # bottom side
+            scale_func(subsurface(c, src_h - c, src_x_side, c), (ret_x_side, c)),
+            (c, ret_h - c),
+        )
+
     if src_y_side > 0:
-        ret_surface.blit( # left side
-            scale_func(subsurface(0, c, c, src_y_side), 
-                       (c, ret_y_side)), 
-            (0, c))
-        ret_surface.blit( # right side
-            scale_func(subsurface(src_w-c, c, c, src_y_side), 
-                       (c, ret_y_side)), 
-            (ret_w-c, c))
-        
+        ret_surface.blit(  # left side
+            scale_func(subsurface(0, c, c, src_y_side), (c, ret_y_side)), (0, c)
+        )
+        ret_surface.blit(  # right side
+            scale_func(subsurface(src_w - c, c, c, src_y_side), (c, ret_y_side)),
+            (ret_w - c, c),
+        )
+
     if src_x_side > 0 and src_y_side > 0:
-        ret_surface.blit( # central area
+        ret_surface.blit(  # central area
             scale_func(
-                subsurface(c, c, src_x_side, src_y_side),
-                (ret_x_side, ret_y_side)), 
-            (c, c))
-        
+                subsurface(c, c, src_x_side, src_y_side), (ret_x_side, ret_y_side)
+            ),
+            (c, c),
+        )
+
     return ret_surface
-    
+
 
 def main():
     pygame.init()
@@ -105,38 +107,43 @@ def main():
     pygame.display.set_caption("9-Patch Scale and Normal Scale Example")
     clock = pygame.Clock()
     font = pygame.Font(None, 30)
-    
+
     original_surface = pygame.Surface((100, 100), pygame.SRCALPHA)
-    pygame.draw.rect(original_surface, "green", original_surface.get_rect(), 0, CORNER_SIZE)
-    
+    pygame.draw.rect(
+        original_surface, "green", original_surface.get_rect(), 0, CORNER_SIZE
+    )
+
     normal_scale = pygame.transform.scale(original_surface, SCALE_SIZE)
-    normal_center = (SCREEN_SIZE[0]/2, SCREEN_SIZE[1]/4)
+    normal_center = (SCREEN_SIZE[0] / 2, SCREEN_SIZE[1] / 4)
     normal_rect = normal_scale.get_rect(center=normal_center)
-    
-    ninepatch = ninepatch_scale(original_surface, SCALE_SIZE, CORNER_SIZE, True, False, None)
-    ninepatch_center = (SCREEN_SIZE[0]/2, SCREEN_SIZE[1]*(3/4))
+
+    ninepatch = ninepatch_scale(
+        original_surface, SCALE_SIZE, CORNER_SIZE, True, False, None
+    )
+    ninepatch_center = (SCREEN_SIZE[0] / 2, SCREEN_SIZE[1] * (3 / 4))
     ninepatch_rect = ninepatch.get_rect(center=ninepatch_center)
-    
+
     text_normal = font.render("Normal Scale", True, "black")
     text_normal_rect = text_normal.get_rect(center=normal_center)
     text_9patch = font.render("9-Patch Scale", True, "black")
     text_9patch_rect = text_9patch.get_rect(center=ninepatch_center)
-    
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-                
+
         screen.fill(0)
-        
+
         screen.blit(normal_scale, normal_rect)
         screen.blit(ninepatch, ninepatch_rect)
         screen.blit(text_normal, text_normal_rect)
         screen.blit(text_9patch, text_9patch_rect)
-        
+
         pygame.display.flip()
         clock.tick(60)
+
 
 if __name__ == "__main__":
     main()
