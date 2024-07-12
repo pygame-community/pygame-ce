aabeed961b48817d97a54a9149428930f24d3254
Ankith <itsankith26@gmail.com>
2024-07-12 21:54:50 +0530

    Support iterable in display.update

--- buildconfig/stubs/pygame/display.pyi
+++ buildconfig/stubs/pygame/display.pyi
@@ -1,4 +1,4 @@
-from typing import Dict, List, Optional, Tuple, Union, overload, Literal
+from typing import Dict, List, Optional, Tuple, Union, overload, Literal, Iterable
 from typing_extensions import deprecated # added in 3.13
 
 from pygame.constants import FULLSCREEN
@@ -48,7 +48,7 @@ def get_surface() -> Surface: ...
 def flip() -> None: ...
 @overload
 def update(
-    rectangle: Optional[Union[RectValue, Sequence[Optional[RectValue]]]] = None, /
+    rectangle: Optional[Union[RectValue, Iterable[Optional[RectValue]]]] = None, /
 ) -> None: ...
 @overload
 def update(x: int, y: int, w: int, h: int, /) -> None: ...
