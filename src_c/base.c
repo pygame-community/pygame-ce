da19260bf72f40486fd9bf4955c343306da51390
Starbuck5 <46412508+Starbuck5@users.noreply.github.com>
2024-07-25 21:55:36 -0700

    Use PyObject_CallNoArgs where applicable
    * PyObject_CallNoArgs was added in 3.9, pythoncapi_compat.h enables support for 3.8.
    * Docs say it's the "most efficient way to call a callable Python object without any argument"

--- src_c/base.c
+++ src_c/base.c
@@ -270,7 +270,7 @@ pg_mod_autoinit(const char *modname)
     }
 
     if (funcobj) {
-        temp = PyObject_CallObject(funcobj, NULL);
+        temp = PyObject_CallNoArgs(funcobj);
         if (temp) {
             Py_DECREF(temp);
             ret = 1;
@@ -305,7 +305,7 @@ pg_mod_autoquit(const char *modname)
         PyErr_Clear();
 
     if (funcobj) {
-        temp = PyObject_CallObject(funcobj, NULL);
+        temp = PyObject_CallNoArgs(funcobj);
         Py_XDECREF(temp);
     }
 
@@ -429,7 +429,7 @@ _pg_quit(void)
             }
 
             if (PyCallable_Check(quit)) {
-                temp = PyObject_CallObject(quit, NULL);
+                temp = PyObject_CallNoArgs(quit);
                 if (temp)
                     Py_DECREF(temp);
                 else
