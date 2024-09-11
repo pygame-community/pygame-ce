9bfdbe0454f192dc6f08500fa4095853f855386b
Ashvin Pai <avpai@umich.edu>
2024-09-11 10:47:43 -0400

    Fix test_inflate_ip__larger.
    This test case was not checking what it was supposed to check (duplicate code of test_inflate_ip__smaller)

--- test/rect_test.py
+++ test/rect_test.py
@@ -801,18 +801,18 @@ class RectTypeTest(unittest.TestCase):
         """The inflate_ip method inflates around the center of the rectangle"""
         r = Rect(2, 4, 6, 8)
         r2 = Rect(r)
-        r2.inflate_ip(-4, -6)
+        r2.inflate_ip(4, 6)
 
         self.assertEqual(r.center, r2.center)
-        self.assertEqual(r.left + 2, r2.left)
-        self.assertEqual(r.top + 3, r2.top)
-        self.assertEqual(r.right - 2, r2.right)
-        self.assertEqual(r.bottom - 3, r2.bottom)
-        self.assertEqual(r.width - 4, r2.width)
-        self.assertEqual(r.height - 6, r2.height)
+        self.assertEqual(r.left - 2, r2.left)
+        self.assertEqual(r.top - 3, r2.top)
+        self.assertEqual(r.right + 2, r2.right)
+        self.assertEqual(r.bottom + 3, r2.bottom)
+        self.assertEqual(r.width + 4, r2.width)
+        self.assertEqual(r.height + 6, r2.height)
 
     def test_inflate_ip__smaller(self):
-        """The inflate method inflates around the center of the rectangle"""
+        """The inflate_ip method inflates around the center of the rectangle"""
         r = Rect(2, 4, 6, 8)
         r2 = Rect(r)
         r2.inflate_ip(-4, -6)
