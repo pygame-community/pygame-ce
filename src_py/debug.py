cc3ce86ebcee45c54dd76642b4d9859e01d37d5f
Dan Lawrence <danintheshed@gmail.com>
2024-10-06 17:10:26 +0100

    Simple implementation of pygame.debug

--- src_py/debug.py
+++ src_py/debug.py
@@ -2,14 +2,15 @@ import os
 import platform
 import warnings
 import pygame
-from pygame.base import get_sdl_version
 
 if "PYGAME_HIDE_SUPPORT_PROMPT" not in os.environ:
     print(
-        f"pygame-ce {pygame.version.ver} (SDL {'.'.join(map(str, get_sdl_version()))}, "
+        f"pygame-ce {pygame.version.ver} (SDL {'.'.join(map(str, pygame.base.get_sdl_version()))}, "
         f"Python {platform.python_version()})"
     )
 
     pygame.print_debug_info()
 
 warnings.filterwarnings("default")
+
+del os, platform, warnings
