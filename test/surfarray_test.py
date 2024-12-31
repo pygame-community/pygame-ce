d0b8e73abc2dad51e869d4826700bcfbffaca0d9
Dan Lawrence <danintheshed@gmail.com>
2024-12-31 09:02:48 +0000

    Revert numpy array().astype(int) fix
    Ankith believes we can fix this better in the library code.

--- test/surfarray_test.py
+++ test/surfarray_test.py
@@ -115,10 +115,10 @@ class SurfarrayModuleTest(unittest.TestCase):
 
     def _fill_array2d(self, arr, surf):
         palette = self.test_palette
-        arr[:5, :6] = array(surf.map_rgb(palette[1])).astype(int)
-        arr[5:, :6] = array(surf.map_rgb(palette[2])).astype(int)
-        arr[:5, 6:] = array(surf.map_rgb(palette[3])).astype(int)
-        arr[5:, 6:] = array(surf.map_rgb(palette[4])).astype(int)
+        arr[:5, :6] = surf.map_rgb(palette[1])
+        arr[5:, :6] = surf.map_rgb(palette[2])
+        arr[:5, 6:] = surf.map_rgb(palette[3])
+        arr[5:, 6:] = surf.map_rgb(palette[4])
 
     def _fill_array3d(self, arr):
         palette = self.test_palette
