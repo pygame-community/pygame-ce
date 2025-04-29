43c338bc0c379a702764540ba5c047298dd27f90
Josip Komljenović <josipkoma@gmail.com>
2025-04-29 14:59:32 +0200

    Refactor typing

--- buildconfig/stubs/gen_stubs.py
+++ buildconfig/stubs/gen_stubs.py
@@ -158,6 +158,7 @@ with open(locals_file, "w") as f:
         f.write(f"{element}: {constant_type}\n")
 
 # copy typing.py to typing.pyi for type checkers
-typing_py_file = pathlib.Path(__file__).parent.parent.parent / "src_py" / "typing.py"
-typing_stub_file = pathlib.Path(__file__).parent / "pygame" / "typing.pyi"
+typing_dir = pathlib.Path(__file__).parent.parent.parent / "src" / "typing"
+typing_py_file = typing_dir / "typing.py"
+typing_stub_file = typing_dir / "typing.pyi"
 shutil.copyfile(typing_py_file, typing_stub_file)
