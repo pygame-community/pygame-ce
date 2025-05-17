1e1436be9b767f06e89052f63a8a9287bf4d7d46
damusss <ricciardi.damiano06@gmail.com>
2025-05-17 10:52:39 +0200

    Remove from module list, keep private

--- buildconfig/stubs/pygame/_render.pyi
+++ buildconfig/stubs/pygame/_render.pyi
@@ -6,6 +6,27 @@ rendering through the Renderer, Texture and Image objects.
 .. versionadded:: 2.5.4
 """
 
+# render.rst contents for the future
+"""
+.. include:: common.txt
+
+:mod:`pygame._render`
+=====================
+
+.. warning::
+   This module isn't ready for prime time yet, it's still in development.
+   These docs are primarily meant to help the pygame developers and
+   super-early adopters who are in communication with the developers.
+   This API will change.
+
+   Also, this module is a C implementation of most of the features of the
+   sdl2_video module. It is currently incomplete.
+
+.. autopgmodule:: pygame._render
+   :members:
+
+"""
+
 from typing import Optional, Protocol, Union, final
 
 from pygame.color import Color
