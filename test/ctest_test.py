89988c96616df8a343e2c4fba049cadaae16a157
Andrew Coffey <andrewryancoffey@hotmail.com>
2025-06-07 09:30:39 -0500

    ctest is now python package

--- test/ctest_test.py
+++ test/ctest_test.py
@@ -1,40 +1,15 @@
-import io
-import os
-import re
-import subprocess
 import unittest
 
+base_ctest = None
+try:
+    import pygame.base_ctest as base_ctest
+except ModuleNotFoundError:
+    pass
 
 class Ctest(unittest.TestCase):
-    def test_run_ctests(self):
-        filename_regex = re.compile(".*run_ctests.*")
-
-        test_dir = os.path.dirname(os.path.abspath(__file__))
-        pg_dir = os.path.join(test_dir, "..")
-
-        test_executable_exists = False
-        test_filename = ""
-        for filename in os.listdir(pg_dir):
-            if re.match(filename_regex, filename):
-                test_filename = filename
-                test_executable_exists = True
-                break
-
-        if test_executable_exists:
-            cwd = os.getcwd()
-            os.chdir(pg_dir)
-
-            with open(f"{cwd}/ctest.log", 'w') as ctest_log:
-                test_result = subprocess.run(
-                    [test_filename], stdout=ctest_log, stderr=subprocess.STDOUT
-                )
-
-            os.chdir(cwd)
-
-            self.assertEqual(test_result.returncode, 0)
-
-        else:
-            unittest.skip("Test Executable Not Available")
+    @unittest.skipIf(base_ctest is None, "base_ctest not built")
+    def test_run_base_ctests(self):
+        self.assertEqual(base_ctest.run_tests(), 0)
 
 
 if __name__ == "__main__":
