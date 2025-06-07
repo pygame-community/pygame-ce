66d8ca3afeb8abf120fad4bb17cf9824f2f237c6
Andrew Coffey <andrewryancoffey@hotmail.com>
2025-06-06 23:42:15 -0500

    Messed with ctest build to try to get it to work on CI... failed

--- dev.py
+++ dev.py
@@ -211,9 +211,9 @@ class Dev:
         sdl3 = self.args.get("sdl3", False)
         coverage = self.args.get("coverage", False)
         ctest = self.args.get("ctest", False)
-        if wheel_dir and coverage:
-            pprint("Cannot pass --wheel and --coverage together", Colors.RED)
-            sys.exit(1)
+        # if wheel_dir and coverage:
+        #     pprint("Cannot pass --wheel and --coverage together", Colors.RED)
+        #     sys.exit(1)
 
         build_suffix = ""
         if debug:
