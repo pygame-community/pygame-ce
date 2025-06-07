cdf2baf75a172c1bf332674c2f8199040b9d992c
Andrew Coffey <andrewryancoffey@hotmail.com>
2025-06-07 09:47:28 -0500

    Formatting

--- ctest/test_common.h
+++ ctest/test_common.h
@@ -3,21 +3,19 @@
 #ifndef TEST_COMMON_H
 #define TEST_COMMON_H
 
-#define RUN_TEST_PG_INTERNAL(TestFunc, TestLineNum, self_arg, null_arg) \
-    {                                                                   \
-        Unity.CurrentTestName = #TestFunc;                              \
-        Unity.CurrentTestLineNumber = TestLineNum;                      \
-        Unity.NumberOfTests++;                                          \
-        if (TEST_PROTECT())                                             \
-        {                                                               \
-            setUp();                                                    \
-            TestFunc(self_arg, null_arg);                               \
-        }                                                               \
-        if (TEST_PROTECT())                                             \
-        {                                                               \
-            tearDown();                                                 \
-        }                                                               \
-        UnityConcludeTest();                                            \
-    }
+#define RUN_TEST_PG_INTERNAL(TestFunc, TestLineNum, self_arg, null_arg)        \
+  {                                                                            \
+    Unity.CurrentTestName = #TestFunc;                                         \
+    Unity.CurrentTestLineNumber = TestLineNum;                                 \
+    Unity.NumberOfTests++;                                                     \
+    if (TEST_PROTECT()) {                                                      \
+      setUp();                                                                 \
+      TestFunc(self_arg, null_arg);                                            \
+    }                                                                          \
+    if (TEST_PROTECT()) {                                                      \
+      tearDown();                                                              \
+    }                                                                          \
+    UnityConcludeTest();                                                       \
+  }
 
 #endif // #ifndef TEST_COMMON_H
