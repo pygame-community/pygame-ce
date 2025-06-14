37c9848c9a2a576dd177f91a0906228318e57fdf
Ankith <itsankith26@gmail.com>
2025-06-14 10:36:51 +0530

    Fix indentation in docs mouse example
    Co-authored-by: aatle <168398276+aatle@users.noreply.github.com>

--- buildconfig/stubs/pygame/mouse.pyi
+++ buildconfig/stubs/pygame/mouse.pyi
@@ -50,19 +50,19 @@ scroll, such as ``which`` (it will tell you what exact mouse device trigger the 
    clock = pygame.time.Clock()
 
    def main():
-      while True:
-         for event in pygame.event.get():
+       while True:
+           for event in pygame.event.get():
                if event.type == QUIT:
-                  pygame.quit()
-                  return
+                   pygame.quit()
+                   return
                elif event.type == MOUSEWHEEL:
-                  print(event)
-                  print(event.x, event.y)
-                  print(event.flipped)
-                  print(event.which)
-                  # can access properties with
-                  # proper notation(ex: event.y)
-         clock.tick(60)
+                   print(event)
+                   print(event.x, event.y)
+                   print(event.flipped)
+                   print(event.which)
+                   # can access properties with
+                   # proper notation(ex: event.y)
+           clock.tick(60)
 
    # Execute game:
    main()
