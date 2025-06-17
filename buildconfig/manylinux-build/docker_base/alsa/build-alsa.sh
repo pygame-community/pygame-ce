b883603f761ab1173468388226bf3b045277529b
Ankith <itsankith26@gmail.com>
2025-06-18 00:28:14 +0530

    Bump mac/manylinux dependencies

--- buildconfig/manylinux-build/docker_base/alsa/build-alsa.sh
+++ buildconfig/manylinux-build/docker_base/alsa/build-alsa.sh
@@ -3,7 +3,7 @@ set -e -x
 
 cd $(dirname `readlink -f "$0"`)
 
-ALSA=alsa-lib-1.2.13
+ALSA=alsa-lib-1.2.14
 curl -sL https://www.alsa-project.org/files/pub/lib/${ALSA}.tar.bz2 > ${ALSA}.tar.bz2
 sha512sum -c alsa.sha512
 
