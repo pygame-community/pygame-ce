03bbbd429128061bfcb09d53d04a7c3bc49393fd
Dan Lawrence <danintheshed@gmail.com>
2025-08-25 20:37:28 +0100

    Fix SURF_GET_AT macro for SDL3

--- src_c/draw.c
+++ src_c/draw.c
@@ -2472,9 +2472,8 @@ draw_line(SDL_Surface *surf, SDL_Rect surf_clip_rect, int x1, int y1, int x2,
     }
     set_and_check_rect(surf, surf_clip_rect, x2, y2, color, drawn_area);
 }
-
-#define SURF_GET_AT(p_color, p_surf, p_x, p_y, p_pixels, p_format, p_pix)    \
-    switch (PG_FORMAT_BytesPerPixel(p_format)) {                             \
+#define SURF_GET_AT(p_color, p_surf, p_x, p_y, p_pixels, p_pix)              \
+    switch (PG_SURF_BytesPerPixel(p_surf)) {                                 \
         case 1:                                                              \
             p_color = (Uint32) *                                             \
                       ((Uint8 *)(p_pixels) + (p_y) * p_surf->pitch + (p_x)); \
@@ -2549,8 +2548,7 @@ flood_fill_inner(SDL_Surface *surf, int x1, int y1, Uint32 new_color,
         goto flood_fill_finished;
     }
 
-    SURF_GET_AT(old_color, surf, x1, y1, (Uint8 *)surf->pixels, surf->format,
-                pix);
+    SURF_GET_AT(old_color, surf, x1, y1, (Uint8 *)surf->pixels, pix);
 
     if (pattern == NULL && old_color == new_color) {
         // not an error, but nothing to do here
@@ -2573,8 +2571,7 @@ flood_fill_inner(SDL_Surface *surf, int x1, int y1, Uint32 new_color,
 
             Uint32 current_color = 0;
 
-            SURF_GET_AT(current_color, surf, x, y, (Uint8 *)surf->pixels,
-                        surf->format, pix);
+            SURF_GET_AT(current_color, surf, x, y, (Uint8 *)surf->pixels, pix);
 
             if (current_color != old_color) {
                 continue;
@@ -2582,7 +2579,7 @@ flood_fill_inner(SDL_Surface *surf, int x1, int y1, Uint32 new_color,
 
             if (pattern != NULL) {
                 SURF_GET_AT(new_color, pattern, x % pattern->w, y % pattern->h,
-                            (Uint8 *)pattern->pixels, pattern->format, pix);
+                            (Uint8 *)pattern->pixels, pix);
             }
 
             // clipping and color mapping have already happened here
