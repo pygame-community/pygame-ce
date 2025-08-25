675fc2183236a352c5a0adcebc3b4b732ab218e5
Ankith <itsankith26@gmail.com>
2025-08-25 11:01:32 +0530

    Build windows deps ourselves

--- buildconfig/manylinux-build/docker_base/portmidi/build-portmidi.sh
+++ buildconfig/manylinux-build/docker_base/portmidi/build-portmidi.sh
@@ -3,10 +3,11 @@ set -e -x
 
 cd $(dirname `readlink -f "$0"`)
 
-PORTMIDI_VER="2.0.4"
+# 2.0.4 has compilation issues on windows, so pin to latest commit on github
+PORTMIDI_VER="7a5de5b7597c46f963d72a83defe7592f901e5f1"
 PORTMIDI="portmidi-${PORTMIDI_VER}"
 
-curl -sL --retry 10 https://github.com/PortMidi/portmidi/archive/refs/tags/v${PORTMIDI_VER}.tar.gz> ${PORTMIDI}.tar.gz
+curl -sL --retry 10 https://github.com/PortMidi/portmidi/archive/$PORTMIDI_VER.tar.gz > ${PORTMIDI}.tar.gz
 sha512sum -c portmidi.sha512
 
 tar xzf ${PORTMIDI}.tar.gz
