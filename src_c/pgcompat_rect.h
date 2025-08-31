379f5b6453ecee3fc2404f5bae29c409b0f01808
Ankith <itsankith26@gmail.com>
2025-08-31 17:57:35 +0530

    SDL3: mouse+key+rect: runtime fixes

--- src_c/pgcompat_rect.h
+++ src_c/pgcompat_rect.h
@@ -8,14 +8,14 @@
 #endif
 
 /* SDL 2.0.22 provides some utility functions for FRects */
-#if !(SDL_VERSION_ATLEAST(2, 0, 22))
+#if !(SDL_VERSION_ATLEAST(2, 0, 22)) || SDL_VERSION_ATLEAST(3, 0, 0)
 
 SDL_bool
 PG_IntersectFRectAndLine(SDL_FRect *rect, float *X1, float *Y1, float *X2,
                          float *Y2);
 #else
 #define PG_IntersectFRectAndLine SDL_IntersectFRectAndLine
-#endif /* !(SDL_VERSION_ATLEAST(2, 0, 22)) */
+#endif /* !(SDL_VERSION_ATLEAST(2, 0, 22)) || SDL_VERSION_ATLEAST(3, 0, 0) */
 
 #define pg_PyFloat_FromFloat(x) (PyFloat_FromDouble((double)x))
 
