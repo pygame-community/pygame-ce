f06fff4b569fb9c7eda5b5e3433e1fbd4f92cf4f
Josip Komljenović <josipkoma@gmail.com>
2026-02-10 12:50:23 +0100

    Update render_test.py

--- test/render_test.py
+++ test/render_test.py
@@ -485,6 +485,10 @@ class TextureTest(unittest.TestCase):
 
 class ImageTest(unittest.TestCase):
     def setUp(self):
+        self.window = pygame.Window(size=(100, 100))
+        self.renderer = _render.Renderer(self.window)
+        self.surface = pygame.Surface((50, 50))
+        self.surface.fill(pygame.Color(80, 120, 160, 255))
         '''self.window = pygame.Window(size=(100, 100))
         self.renderer = _render.Renderer(self.window)
         self.surface = pygame.Surface((50, 50))
