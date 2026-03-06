ec818380b939a69a3869418bce4e8ff7ed22a7a5
Starbuck5 <46412508+Starbuck5@users.noreply.github.com>
2026-03-05 23:09:12 -0800

    Move non deprecated scrap function docs to stubs

--- buildconfig/stubs/pygame/scrap.pyi
+++ buildconfig/stubs/pygame/scrap.pyi
@@ -3,6 +3,41 @@ from typing_extensions import (
     deprecated,  # added in 3.13
 )
 
+def put_text(text: str, /) -> None:
+    """
+    Places text into the clipboard.
+
+    Places the input text into the clipboard. The data should be a string.
+    This is the same clipboard as the legacy scrap API when using ``SCRAP_TEXT``.
+
+    :raises pygame.error: if video mode has not been set_mode
+
+    .. note:: ``pygame.display.set_mode()`` should be called before using the ``scrap`` module
+
+    .. versionadded:: 2.2.0
+    """
+
+def get_text() -> str:
+    """
+    Gets text from the clipboard.
+
+    Gets text from the clipboard and returns it. If the clipboard is empty,
+    returns an empty string. This is the same clipboard as the legacy scrap
+    API when using ``SCRAP_TEXT``.
+
+    .. versionadded:: 2.2.0
+    """
+
+def has_text() -> bool:
+    """
+    Checks if text is in the clipboard.
+
+    Returns ``True`` if the clipboard has a string, otherwise returns ``False``.
+    This is the same clipboard as the legacy scrap API when using ``SCRAP_TEXT``.
+
+    .. versionadded:: 2.2.0
+    """
+
 @deprecated("since 2.2.0. Use the new API instead, which only requires display init")
 def init() -> None: ...
 @deprecated("since 2.2.0. Use the new API instead, which doesn't require scrap init")
@@ -19,6 +54,3 @@ def contains(data_type: str, /) -> bool: ...
 def lost() -> bool: ...
 @deprecated("since 2.2.0. Use the new API instead, which only supports strings")
 def set_mode(mode: int, /) -> None: ...
-def put_text(text: str, /) -> None: ...
-def get_text() -> str: ...
-def has_text() -> bool: ...
