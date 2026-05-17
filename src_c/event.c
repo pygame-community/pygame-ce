d999e346109f2702ee8db8223c2ea75058b6ec4d
Michael Lamertz <34611868+DickerDackel@users.noreply.github.com>
2026-05-17 12:20:19 +0200

    `pos` attribute added to `MOUSEWHEEL` event (#3783)

--- src_c/event.c
+++ src_c/event.c
@@ -1380,6 +1380,18 @@ dict_from_event(SDL_Event *event)
                 dict, "touch",
                 PyBool_FromLong((event->wheel.which == SDL_TOUCH_MOUSEID)));
 
+#if SDL_VERSION_ATLEAST(3, 0, 0)
+            obj = pg_tuple_couple_from_values_int((int)event->wheel.mouse_x,
+                                                  (int)event->wheel.mouse_y);
+#elif SDL_VERSION_ATLEAST(2, 26, 0)
+            obj = pg_tuple_couple_from_values_int((int)event->wheel.mouseX,
+                                                  (int)event->wheel.mouseY);
+#else
+        obj = Py_NewRef(Py_None);
+
+#endif /* SDL_VERSION_ATLEAST(2, 26, 0) */
+            _pg_insobj(dict, "pos", obj);
+
             break;
         case SDL_TEXTINPUT:
             /* https://wiki.libsdl.org/SDL_TextInputEvent */
