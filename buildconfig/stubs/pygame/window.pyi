9d2712bc81d00919057243f7043ca1d4d83f8757
damusss <ricciardi.damiano06@gmail.com>
2026-09-03 00:13:50 +0200

    Improve sentence wording

--- buildconfig/stubs/pygame/window.pyi
+++ buildconfig/stubs/pygame/window.pyi
@@ -17,8 +17,8 @@ class Window:
     window class will continue to be developed, and we're excited to share
     the new functionality this class offers.
 
-    .. note:: Every window property, method and operation can only be
-        performed on the main thread the application started in.
+    .. note:: You can only interact with a Window (get/set properties,
+        call methods, etc.) from your application's main thread.
 
     :param str title: The title of the window.
     :param (int, int) size: The size of the window, in screen coordinates.
