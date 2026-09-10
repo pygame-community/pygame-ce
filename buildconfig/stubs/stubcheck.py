34e61d118c231f47bb0e23bccbb064b1b0e45e8a
aatle <168398276+aatle@users.noreply.github.com>
2026-09-09 19:45:38 -0700

    Fix mypy checking and stub errors

--- buildconfig/stubs/stubcheck.py
+++ buildconfig/stubs/stubcheck.py
@@ -2,13 +2,14 @@
 A helper script to run mypy stubtest on the stubs directory
 """
 
-import os
 import subprocess
 import sys
 
 from pathlib import Path
 
 STUBS_BASE_DIR = Path(__file__).parent
+ROOT_DIR = STUBS_BASE_DIR.parents[1]
+ALLOW_LIST = STUBS_BASE_DIR / "mypy_allow_list.txt"
 
 
 def typing_check():
@@ -24,17 +25,12 @@ def typing_check():
         print("ERROR: could not validate typing.py, make sure you have mypy installed")
         return
 
-    mypy_args = [*mypy_version_args, "typing_sample_app.py"]
+    mypy_args = [*mypy_version_args, "."]
     cmd = " ".join(mypy_args)
     print(f"Using mypy invocation: `{cmd}` (version: {version})")
-    prev_dir = os.getcwd()
-    try:
-        os.chdir(STUBS_BASE_DIR)
-        returncode = subprocess.run([*mypy_args]).returncode
-        if returncode != 0:
-            raise RuntimeError(f"mypy process finished with unsuccessful return code {returncode}")
-    finally:
-        os.chdir(prev_dir)
+    returncode = subprocess.run([*mypy_args], cwd=ROOT_DIR).returncode
+    if returncode != 0:
+        raise RuntimeError(f"mypy process finished with unsuccessful return code {returncode}")
 
 def stubs_check():
     """
@@ -50,22 +46,20 @@ def stubs_check():
 
         cmd = " ".join(stubtest)
         print(f"Using stubtest invocation: `{cmd}` (version: {version})")
-        prev_dir = os.getcwd()
-        try:
-            os.chdir(STUBS_BASE_DIR)
-            sys.exit(
-                subprocess.run(
-                    [
-                        *stubtest,
-                        "pygame",
-                        "--ignore-disjoint-bases",
-                        "--allowlist",
-                        "mypy_allow_list.txt",
-                    ]
-                ).returncode
-            )
-        finally:
-            os.chdir(prev_dir)
+        sys.exit(
+            subprocess.run(
+                [
+                    *stubtest,
+                    "pygame",
+                    "--ignore-disjoint-bases",
+                    "--mypy-config-file",
+                    "pyproject.toml",
+                    "--allowlist",
+                    str(ALLOW_LIST),
+                ],
+                cwd=ROOT_DIR,
+            ).returncode
+        )
 
     print("ERROR: Could not find a valid stubtest program.")
     print("Make sure you have mypy installed.")
